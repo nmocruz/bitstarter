a2fe316b99c1ff72b27936c33bcfc1931a0c337d
Nuno Cruz <nmocruz@gmail.com>
2013-07-09 06:55:05 +0000

    add grader

--- grader.js
+++ grader.js
@@ -24,9 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_DEFAULT = "http://powerful-stream-1308.herokuapp.com/";
+ 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -36,16 +38,17 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtml = function(html) {
+    return cheerio.load(html);
 };
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(data, checksfile) {
+    $ = cheerioHtml(data);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +64,29 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+logChecks = function(data, checks){
+    var checkJson = checkHtml(data, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Url to html', URL_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+	if(program.url){
+		rest.get(program.url).on('complete',function(result, response){
+		    logChecks(result, program.checks);
+		});
+	}
+	else{
+	   logChecks(fs.readFileSync(program.file), program.checks);
+	}
+		
+    
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.logChecks = logChecks;
 }
